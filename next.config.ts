8cc9cc1d7235ef3c175f4417124ddbe3ba6a4a7c
Davide Rossi <rossidavide108@gmail.com>
2025-09-29 22:49:19 +0200

    wip: update next config js

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,14 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`
+      }
+    ];
+  }
 };
 
 export default nextConfig;
